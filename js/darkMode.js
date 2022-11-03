7c2558aaabfcd14cd2d149f0eb6e6a6f8d995684
Ana Kariny <anakariny10@gmail.com>
2022-11-03 20:23:05 -0300

    finalizando o dark mode

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,24 +1,24 @@
 const body = document.body;
 const botaoDarkMode = document.querySelector('[data-botao-darkMode]');
+const lua = document.querySelector('[data-emoji-lua]');
+const sol = document.querySelector('[data-emoji-sol]');
 const app = document.querySelector('[data-app]');
 const botaoForm = document.querySelector('[data-botao-form]');
 const listaItem = document.querySelector('[data-lista-item]');
 const botaoConcluir = document.querySelector('[data-botao-concluir]');
 const botaoExcluir = document.querySelector('[data-botao-excluir]');
-const rodape = document.querySelector('[data-rodape]');
 
 function darkMode() {
     body.classList.toggle("darkMode");
+    botaoDarkMode.classList.toggle("darkMode__botao-cabecalho");
+    lua.classList.toggle("darkMode__lua");
+    sol.classList.toggle("darkMode__sol");
     app.classList.toggle("darkMode__app");
     botaoForm.classList.toggle("darkMode__botao-form");
     listaItem.classList.toggle("darkMode__lista-item");
     botaoConcluir.classList.toggle("darkMode__lista-botao");
     botaoExcluir.classList.toggle("darkMode__lista-botao");
-    rodape.classList.toggle("darkMode__rodape");
 }; 
 
 
 botaoDarkMode.addEventListener("click", darkMode);
-
-/*DEIXAR O CODIGO MAIS FLUIDO E MUDAR A APRARENCIA DO BOTAO*/
-// FAZER TB A MANIPULACAO DO DOM PARA INSERIR ITENS NA LISTA
